25b5bbaa5021c9a228b587d6f6de38a44a6ce5e6
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2023-09-26 15:57:45 +0530

    hardcode login authorize

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component,OnInit} from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,7 @@ export class LoginComponent implements OnInit {
   password = "";
   errorMsg = "";
 
-  constructor() { }
+  constructor(private auth: AuthService,private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -22,6 +24,13 @@ export class LoginComponent implements OnInit {
       this.errorMsg="Password is required";
     }else{
       this.errorMsg="";
+      let res=this.auth.login(this.username,this.password);
+      if(res===200){
+        this.router.navigate(['home'])
+      }
+      if(res===403){
+        this.errorMsg="Invalid credentials"
+      }
     }
   }
 
